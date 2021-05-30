3621116ea66fb865ad033a5243c68f5d0cc450a6
Eduardo-H <edu-henrique2014@hotmail.com>
2021-05-29 23:38:01 -0300

    utterances configuration modified

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -58,7 +58,7 @@ export default function Post({
     let anchor = document.getElementById("inject-comments-for-uterances");
     script.setAttribute("src", "https://utteranc.es/client.js");
     script.setAttribute("crossorigin","anonymous");
-    script.setAttribute("async", "true");
+    script.setAttribute("async", true);
     script.setAttribute("repo", "Eduardo-H/galaxy-explorer-comments");
     script.setAttribute("issue-term", "pathname");
     script.setAttribute( "theme", "github-dark");
